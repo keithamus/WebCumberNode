5d86371ed86e087c401177ab84c7501a5ce1d428
Dennis Lo <lo.dennis@gmail.com>
2015-01-30 16:37:20 +0000

    Adding chainable WebDriverIO methods, code formatting and read me clarifications

--- features/step_definitions/do_a_google_search.js
+++ features/step_definitions/do_a_google_search.js
@@ -1,32 +1,55 @@
+'use strict';
+
 module.exports = function () {
-    console.log('Do a Google Search Steps Loaded');
-    this.World = require('../support/world');
+  console.log('Do a Google Search Steps Loaded');
+  this.World = require('../support/world');
 
-    /* "<Then> I should see search results" */
-    this.Then(/^I should see search results$/, function (callback) {
+  /* "<Then> I should see search results" */
+  this.Then(/^I should see search results$/, function (callback) {
 
-        var inputId = '#resultStats';
+    var inputId = '#resultStats';
 
-        this.waitFor(inputId, 3000, function (err) {
+    this.waitFor(inputId, 5000, function (err) {
 
-            if (err) {
-                return callback.fail(new Error('Element ' + inputId + ' was not found after 3s'));
-            }
+      if (err) {
+        return callback.fail(new Error('Element ' + inputId + ' was not found after 5s'));
+      }
 
-            this.getText(inputId, function (err, text) {
+      this.getText(inputId, function (err, text) {
 
-                text = (''+text).match(/^About ([\d,]+) .*$/);
-                text = parseInt(text[1].replace(/,/g,''), 10);
+        text = ('' + text).match(/^About ([\d,]+) .*$/);
+        text = parseInt(text[1].replace(/,/g, ''), 10);
 
-                // Fail this test if the search results are null
-                if (text === 0 || isNaN(text))
-                    return callback.fail(new Error('Expected to see some results, but saw 0 (captured result count is ' + number + ')'));
+        // Fail this test if the search results are null
+        if (text === 0 || isNaN(text))
+          return callback.fail(new Error('Expected to see some results, but saw 0 (captured result count is ' + number + ')'));
 
-                // Pass and say about it.
-                console.log( '(Saw ' + text + ' results)');
-                return callback();
-            });
+        // Pass and say about it.
+        console.log('(Saw ' + text + ' results)');
+        return callback();
+      });
 
-        });
     });
+  });
+
+  /* "<Then> I should see search results (chainable version)" */
+  this.Then(/^I should see search results \(chainable version\)$/, function (callback) {
+    var inputId = '#resultStats';
+
+    this.waitFor(inputId, 5000).getText(inputId, function (err, text) {
+
+      text = ('' + text).match(/^About ([\d,]+) .*$/);
+      text = parseInt(text[1].replace(/,/g, ''), 10);
+
+      // Fail this test if the search results are null
+      if (text === 0 || isNaN(text))
+        return callback.fail(new Error('Expected to see some results, but saw 0 (captured result count is ' + number + ')'));
+
+      // Pass and say about it.
+      console.log('(Saw ' + text + ' results)');
+      return callback();
+    });
+
+  });
+
 };
--- features/support/world.js
+++ features/support/world.js
@@ -1,39 +1,42 @@
-module.exports = function(callback) {
+'use strict';
 
-    if ( callback ) {
+var WebDriverIO = require('webdriverio');
 
-        // Get our test properties
-        var PROPERTIES = require('./testproperties.json');
+module.exports = function (callback) {
 
-        // Override any testproperty from command line with --[PROPERTY]=[VALUE]
-        process.argv.forEach(function (arg) {
-            arg = arg.match(/^--([A-Za-z]+)=(.*)/);
+  if (callback) {
 
-            // Only look for --[PROPERTY] style args, everything else can be forgotten
-            if (arg == null || !arg[1]) return;
+    // Get our test properties
+    var PROPERTIES = require('./testproperties.json');
 
+    // Override any testproperty from command line with --[PROPERTY]=[VALUE]
+    process.argv.forEach(function (arg) {
+      arg = arg.match(/^--([A-Za-z]+)=(.*)/);
 
-            // If the JSON file has the argument to override, then override it.
-            if (PROPERTIES.hasOwnProperty(arg[1])) {
-                PROPERTIES[arg[1]] = arg[2];
+      // Only look for --[PROPERTY] style args, everything else can be forgotten
+      if (arg == null || !arg[1]) return;
 
-            // Otherwise proclaim that it is an unrecognised argument
-            } else {
-                console.log('Unrecognised argument ' + arg[1]);
-            }
+      // If the JSON file has the argument to override, then override it.
+      if (PROPERTIES.hasOwnProperty(arg[1])) {
+        PROPERTIES[arg[1]] = arg[2];
 
-        });
+        // Otherwise proclaim that it is an unrecognised argument
+      } else {
+        console.log('Unrecognised argument ' + arg[1]);
+      }
 
-        console.log('Loading browser ' + PROPERTIES.browser);
-        var browser = require("webdriverio").remote({
-            logLevel: PROPERTIES.logLevel || 'none',
-            host: PROPERTIES.host,
-            desiredCapabilities: {
-                browserName: PROPERTIES.browser || 'firefox'
-            }
-        });
+    });
 
-        // Inject the browser into the callback, we don't need anything else
-        callback(browser);
-    }
+    console.log('Loading browser ' + PROPERTIES.browser);
+    var browser = WebDriverIO.remote({
+      logLevel: PROPERTIES.logLevel || 'none',
+      host: PROPERTIES.host,
+      desiredCapabilities: {
+        browserName: PROPERTIES.browser || 'firefox'
+      }
+    });
+
+    // Inject the browser into the callback, we don't need anything else
+    callback(browser);
+  }
 };
